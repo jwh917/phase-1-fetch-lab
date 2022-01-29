24b8e5f53fcd7e37a01ad15c2a128dcc5f94c619
Julien Hnery <jwh_917@yahoo.com>
2022-01-29 17:14:48 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const booksRender = fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
+  return booksRender
+  //console.log('before fetch returns')
 }
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  //const main = document.querySelector("main");
+
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
@@ -13,5 +20,9 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+//console.log('after get Books');
   fetchBooks();
 });
+
+
+
